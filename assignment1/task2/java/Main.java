afcc57606aad66b026a4cccaea28755f7208eedd
Anders <a-tunglan@hotmail.com>
2025-01-13 15:05:34 +0100

    removed artifacts

--- assignment1/task2/java/Main.java
+++ assignment1/task2/java/Main.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
       int[] test = {3, 2, 1};
-      SortingAlgorithms.heap_sort(test);
-      // SortingAlgorithms.insertion_sort(test);
+      // SortingAlgorithms.heap_sort(test);
+      SortingAlgorithms.insertion_sort(test);
       System.out.println(Arrays.toString(test));
     }
   }
