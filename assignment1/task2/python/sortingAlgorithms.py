f53f19de5018c803dfb7e40375e1039496514e1e
Anders <a-tunglan@hotmail.com>
2025-01-15 14:43:40 +0100

    removed unused algorithms and files

--- assignment1/task2/python/sortingAlgorithms.py
+++ assignment1/task2/python/sortingAlgorithms.py
@@ -1,6 +1,5 @@
 # Insertion sort
 
-
 def insertion_sort(n):
     for j in range(1, len(n)):
         key = n[j]
@@ -9,49 +8,6 @@ def insertion_sort(n):
             n[i+1] = n[i]
             i = i - 1
         n[i + 1]  = key
-
-
-# # Merge Sort
-# def merge(A, p, q, r):
-
-#     left = A[p:q+1]
-#     right = A[q+1:r+1]
-
-#     i = 0
-#     j = 0
-#     k = p
-
-#     while i < len(left) and j < len(right):
-#         if left[i] <= right[j]:
-#             A[k] = left[i]
-#             i += 1
-#         else:
-#             A[k] = right[j]
-#             j += 1
-#         k += 1
-    
-#     while i < len(left):
-#         A[k] = left[i]
-#         i += 1
-#         k += 1
-#     while j < len(right):
-#         A[k] = right[j]
-#         j += 1
-#         k += 1
-
-
-# def merge_sort_internal(A, p, r):
-#     if p >= r: # Zero or one element
-#         return
-#     q = (p + r)//2
-#     merge_sort_internal(A, p, q) # Left 
-#     merge_sort_internal(A, q+1, r) # Right
-#     merge(A, p, q, r)
-
-# # Simpler calling without having to specify the range when calling
-# def merge_sort(array):
-#     merge_sort_internal(array, 0, len(array))
-
 
 # Heap sort
 
@@ -85,8 +41,6 @@ def max_heapify(A:Heap, i):
         A.array[i], A.array[largest] = A.array[largest], A.array[i]
         max_heapify(A, largest)
 
-
-
 def build_max_heap(A:Heap, n):
     A.heap_size = n
     for i in range(n//2, -1, -1): # Down to and including 0
@@ -102,28 +56,3 @@ def heap_sort_internal(A:Heap, n):
 def heap_sort(array):
     heap = Heap(array=array)
     heap_sort_internal(heap, len(heap.array)-1)
-
-
-# # Quick Sort
-
-# def partition(A, p, r):
-#     x = A[r]
-#     i = p - 1
-
-#     for j in range(p, r):
-#         if A[j] <= x:
-#             i += 1
-#             A[i], A[j] = A[j], A[i]
-#     A[i+1], A[r] = A[r], A[i+1]
-#     return i + 1
-
-
-# def quick_sort_internal(A, p, r):
-#     if p < r:
-#         q = partition(A, p, r)
-#         quick_sort_internal(A, p, q-1) # recursive low side
-#         quick_sort_internal(A, q+1, r) # recursive high side
-
-# # For easier usage
-# def quick_sort(array):
-#     quick_sort_internal(array, 0, len(array)-1)
--- assignment1/task2/python/test.py
+++
@@ -1,4 +0,0 @@
-from sortingAlgorithms import heap_sort
-array = [1, 2, 3, 4, 5]
-heap_sort(array)
-print(array)
